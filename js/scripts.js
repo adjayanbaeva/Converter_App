175bdd8b85380a4169b04d4240b03037fe257db6
Guest User <Guest@epicodus-5.local>
2018-01-31 08:26:07 -0800

    :add front-end and modified backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,25 @@
-
-var word1 = "Beep!";
-var word2 = "Boop!"
-var word3 = "I'm sorry, Dave. I'm afraid I can't do that."
-
-
-function numbersToConvert(inputNumber){
-  if (typeof inputNumber !== "number" || inputNumber.length == 0){
-    return;
+function numbersToConvert(inputNumber) {
+  var result = [];
+  var numbersInString=inputNumber.toString()
+  if(inputNumber%3===0) {
+    result += "I'm sorry, Dave";
   }
-  var digits = inputNumber.toString().split("").map((item,index) => parseInt(item));
-  for (var index = 0; index < digits.length; index++) {
-    if(index===0){
-      return word1;
-    } else if (index===1){
-      return word2;
-    } else if (index%3===0){
-      return word3;
-    }
+  else if (numbersInString.includes("1")) {
+    result += "Boop!";
+  }
+  else if (numbersInString.includes("0")) {
+    result += "Beep!";
+  }
 
-    }
-    }
+  return result;
+};
+
 
 $(document).ready(function(){
-  $("#number-converter").submit(function(event){
+  $("form#number-converter").submit(function(event){
     event.preventDefault();
-    // var digitsToConvert = $("#input-number").val();
-    // var convertedDigit = numbersToConvert(digitsToConvert);
-    // $("#output").text("");
-    // if(convertedDigit){
-    //   $("#output").append(digitsToConvert );
-    //   $("#output").append(convertedDigit);
-    // }else{
-    //   $("#output").append("Please enter some valid number");
-    // }
-    // $("#input-number").val("")
-
-  });
-});
+     var digitsToConvert = $("#inputNumber").val();
+    //  var convertedDigit = digit(inputNumber);
+     $("#output").text(numbersToConvert(digitsToConvert));
+   });
+ });
